a966ad51fb7df4ea0648c7f48df026511cb1b651
Bazinga <ki5syn333@gmail.com>
2026-03-01 21:40:03 +0300

    forth commit_

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-function info() {
+export function info() {
   const m = new Map();
   m.set('name', 'ajs');
   m.set('version', '1.0.0');
   return m;
-}
-
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = { info };
-}
-
-if (typeof window !== 'undefined') {
-  window.ajs = { info };
 }
